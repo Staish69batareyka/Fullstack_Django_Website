976938dcc3f830dab00f4523e3ba9b065aea8611
Staish69Batareyka <anastaish.batareyka@yandex.ru>
2026-02-02 21:19:07 +0300

    feat: create full pages

--- website/main/views.py
+++ website/main/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def index(response):
-    return HttpResponse('<h1>Проверка связи</h1>')
+def index(request):
+    return render(request, 'main/index.html') # Обращаемся к файлу из templates
 
-def about(response):
-    return HttpResponse('<h1>Это страница про нас</h1>')
+def about(request):
+    return render(request, 'main/about.html')
