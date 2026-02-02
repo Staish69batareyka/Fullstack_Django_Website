2e4da4d8f5e3dcfeff492701a561c39dd39bbea2
Staish69Batareyka <anastaish.batareyka@yandex.ru>
2026-02-02 21:11:47 +0300

    feat: create new url path for new text

--- website/main/views.py
+++ website/main/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(response):
     return HttpResponse('<h1>Проверка связи</h1>')
+
+def about(response):
+    return HttpResponse('<h1>Это страница про нас</h1>')
