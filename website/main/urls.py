152f1d71529a7e50bbdbfb459a069441a01631b3
Staish69Batareyka <anastaish.batareyka@yandex.ru>
2026-02-09 17:00:03 +0300

    feat: add simple roating

--- website/main/urls.py
+++ website/main/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name = 'main'
+
 urlpatterns = [
-    path('', views.popular_list)
+    path('',
+         views.popular_list,
+         name='popular_list'),
+
+    path('<slug:slug>/',
+         views.product_detail,
+         name='product_detail')
+
 ]
